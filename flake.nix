7774373a41cfeb9ad297d117a094d25c3e188724
Pablo Fraile Alonso <pablo@pablofraile.net>
2025-09-29 13:03:18 +0200

    fix version

--- flake.nix
+++ flake.nix
@@ -60,7 +60,7 @@
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          python = pkgs.python3;
+          python = pkgs.python312;  # Because nvidia flare doesn't support python313 yet...
         in
         (pkgs.callPackage pyproject-nix.build.packages {
           inherit python;
@@ -79,25 +79,29 @@
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          pythonSet = pythonSets.${system}.overrideScope editableOverlay;
-          virtualenv = pythonSet.mkVirtualEnv name workspace.deps.all;
+          # pythonSet = pythonSets.${system}.overrideScope editableOverlay;
+          # virtualenv = pythonSets.${system}.mkVirtualEnv "${name}-dev" workspace.deps.all;
         in
         {
           default = pkgs.mkShell {
+            buildInputs = [
+              (pythonSets.${system}.mkVirtualEnv name workspace.deps.default)
+            ];
             packages = [
-              virtualenv
+              # virtualenv
+              # pythonSets.${system}.mkVirtualEnv name workspace.deps.default
               pkgs.uv
             ];
             env = {
               UV_NO_SYNC = "1";
-              UV_PYTHON = pythonSet.python.interpreter;
+              # UV_PYTHON = pythonSet.python.interpreter;
               UV_PYTHON_DOWNLOADS = "never";
             };
             shellHook = ''
-              # unset PYTHONPATH
-              # export REPO_ROOT=$(git rev-parse --show-toplevel)
-              . ${virtualenv}/bin/activate
+              unset PYTHONPATH
+              export REPO_ROOT=$(git rev-parse --show-toplevel)
             '';
+            # . ${virtualenv}/bin/activate
           };
         }
       );
